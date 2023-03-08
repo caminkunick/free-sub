f1b9db3ceab005948c9b0115bbca02cabf5e2ef2
caminkunick <caminkunick@gmail.com>
2023-03-09 02:14:35 +0700

    defaultTheme: changed

--- provider/default.theme.ts
+++ provider/default.theme.ts
@@ -1,20 +1,46 @@
-import { ThemeOptions } from "@mui/material";
+import { PaletteColor, PaletteColorOptions, ThemeOptions } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    neutral: PaletteColor;
+  }
+  interface PaletteOptions {
+    neutral?: PaletteColorOptions;
+  }
+}
 
 declare module "@mui/material/styles/createMixins" {
   interface Mixins {
     sidebar: CSSProperties;
     absoluteFluid: CSSProperties;
-    flexMiddle: CSSProperties;
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    neutral: true;
   }
 }
 
 export const defaultTheme = (dark?: boolean): ThemeOptions => ({
   mixins: {
     sidebar: {
-      width: 252
-    }
+      width: 252,
+    },
+    absoluteFluid: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+    },
   },
   palette: {
+    neutral: {
+      main: grey[600],
+      contrastText: "#fff",
+    },
     mode: dark ? "dark" : "light",
   },
   components: {
@@ -22,16 +48,16 @@ export const defaultTheme = (dark?: boolean): ThemeOptions => ({
       styleOverrides: {
         sizeMedium: {
           width: 48,
-          height: 48
-        }
-      }
+          height: 48,
+        },
+      },
     },
     MuiListItemIcon: {
       styleOverrides: {
         root: {
-          minWidth: 36
-        }
-      }
-    }
-  }
+          minWidth: 36,
+        },
+      },
+    },
+  },
 });
