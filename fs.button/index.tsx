bad58e717d26a6b8114a87d13c1b3b68eedd93a0
caminkunick <caminkunick@gmail.com>
2023-03-09 00:26:05 +0700

    FSButton: changed

--- fs.button/index.tsx
+++ fs.button/index.tsx
@@ -1,11 +1,87 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faPlus,
+  faTrash,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button } from "@mui/material";
+import { Button, ButtonProps } from "@mui/material";
 
 export const FSButton = {
-  Add: () => (
-    <Button variant="outlined" startIcon={<FontAwesomeIcon icon={faPlus} />}>
+  Add: (props: ButtonProps) => (
+    <Button
+      variant="outlined"
+      startIcon={<FontAwesomeIcon icon={faPlus} />}
+      {...props}
+    >
       Add
+    </Button>
+  ),
+  Ok: (props: ButtonProps) => (
+    <Button
+      fullWidth
+      variant="contained"
+      disableElevation
+      startIcon={<FontAwesomeIcon icon={faCheck} />}
+      size="large"
+      type="submit"
+      {...props}
+    >
+      Ok
+    </Button>
+  ),
+};
+
+export const FSPopBtn = {
+  Ok: (props: ButtonProps) => (
+    <Button
+      fullWidth
+      variant="contained"
+      disableElevation
+      startIcon={<FontAwesomeIcon icon={faCheck} />}
+      size="large"
+      type="submit"
+      {...props}
+    >
+      Ok
+    </Button>
+  ),
+  Confirm: (props: ButtonProps) => (
+    <Button
+      fullWidth
+      variant="contained"
+      disableElevation
+      startIcon={<FontAwesomeIcon icon={faCheck} />}
+      size="large"
+      type="submit"
+      {...props}
+    >
+      Confirm
+    </Button>
+  ),
+  Remove: (props: ButtonProps) => (
+    <Button
+      fullWidth
+      variant="contained"
+      disableElevation
+      startIcon={<FontAwesomeIcon icon={faTrash} />}
+      size="large"
+      type="submit"
+      color="error"
+      {...props}
+    >
+      Remove
+    </Button>
+  ),
+  Cancel: (props: ButtonProps) => (
+    <Button
+      fullWidth
+      startIcon={<FontAwesomeIcon icon={faXmark} />}
+      size="large"
+      type="button"
+      {...props}
+    >
+      Cancel
     </Button>
   ),
 };
