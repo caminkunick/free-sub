92b141fd90a111d99abc49655eb03156174b7e30
caminkunick <caminkunick@gmail.com>
2023-03-08 19:23:36 +0700

    XDataGrid: added

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from "@fortawesome/react-fontawesome";
 export * from "@fortawesome/free-solid-svg-icons";
+export * as xdatagrid from "@mui/x-data-grid";
 export * from "@mui/material";
 export * from "./content.header";
 export * from "./fs.button";
