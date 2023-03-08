47fa6fe36a9146f9253f7b4668024e7006b0a8bf
caminkunick <caminkunick@gmail.com>
2023-03-09 00:27:08 +0700

    genColumn: created

--- table.grid/index.tsx
+++ table.grid/index.tsx
@@ -1,11 +1,80 @@
-import { Box } from "@mui/material";
-import { DataGrid, DataGridProps } from "@mui/x-data-grid";
+import { Box, Link as MLink } from "@mui/material";
+import {
+  DataGrid,
+  DataGridProps,
+  GridColDef,
+  GridRowId,
+} from "@mui/x-data-grid";
+import Link from "next/link";
+import { FSDate } from "../ctrl/date";
+import { FSAction } from "../fs.action";
 
 export type TableGridProps = DataGridProps;
 export const TableGrid = (props: TableGridProps) => {
   return (
-    <Box sx={{height:360}}>
-      <DataGrid {...props} />
+    <Box sx={{ height: 360 }}>
+      <DataGrid
+        initialState={{
+          sorting: { sortModel: [{ field: "datemodified", sort: "desc" }] },
+        }}
+        disableRowSelectionOnClick
+        {...props}
+      />
     </Box>
   );
+};
+
+export const genColumn = {
+  action: <T extends unknown>(
+    actions?: Partial<
+      Record<
+        "onEdit" | "onRemove" | "onRedo" | "onRemoveTrash",
+        (row: T) => void
+      >
+    >
+  ): GridColDef => ({
+    field: "action",
+    headerName: " ",
+    align: "center",
+    width: 64,
+    sortable: false,
+    filterable: false,
+    disableColumnMenu: false,
+    renderCell: ({ row }) =>
+      row.visibility === "trash" ? (
+        <></>
+      ) : (
+        <>
+          {actions?.onEdit && (
+            <FSAction.Edit onClick={() => actions.onEdit?.(row)} />
+          )}
+          {actions?.onRemove && (
+            <FSAction.Remove onClick={() => actions.onRemove?.(row)} />
+          )}
+        </>
+      ),
+  }),
+  title: (to?: string | ((id: GridRowId) => string)): GridColDef => ({
+    field: "title",
+    headerName: "Title",
+    renderCell: ({ value, id }) =>
+      to ? (
+        <MLink
+          component={Link}
+          href={to instanceof Function ? to(id) : to + `/${id}`}
+        >
+          {value}
+        </MLink>
+      ) : (
+        value
+      ),
+    width: 280,
+  }),
+  date: (): GridColDef => ({
+    field: "datemodified",
+    headerName: "Date",
+    valueGetter: ({ value }) => new FSDate(value).value,
+    renderCell: ({ value }) => new FSDate(value).toString(),
+    width: 240,
+  }),
 };
